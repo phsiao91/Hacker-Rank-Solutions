a4f0603043bb9c0173aaf5147ba2b644bc50969e
Phing An Hsiao <phinganhsiao@gmail.com>
2021-12-28 23:06:35 -0500

    dec 28

---
+++ RomanToInt.js
@@ -0,0 +1,31 @@
+
+
+
+const romanToInt = (s) => {
+    const roman = {
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    }
+    let total = 0
+    for(let i = 0; i < s.length; i++){
+        let currentInt = roman[s.charAt(i)];
+        let nextInt = roman[s.charAt(i+1)];
+        if(nextInt){
+            if(currentInt >= nextInt){
+                total += currentInt
+            }
+            else{
+                total += (nextInt - currentInt)
+                i++
+            }
+        } else{
+            total += currentInt
+        }
+    }
+    return total;
+};
