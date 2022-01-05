5f2f32de35d307afe412d9eb8a10bda9b4b9da51
Phing An Hsiao <phinganhsiao@gmail.com>
2022-01-04 20:44:38 -0500

    04Jan22

---
+++ PushUpUnique.js
@@ -0,0 +1,15 @@
+
+
+
+const pushUpUnique = (nums) => {
+    if(!nums.length) return 0
+   let i = 0
+   
+   for(let j = 1; j < nums.length; j++){
+       if (nums[i] !== nums[j]){
+           i++
+           nums[i] = nums[j]
+       }
+   }
+    return i+1
+}; 
