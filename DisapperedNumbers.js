e47836d391c5241e91a691e7cf521db51fb5e53a
Phing An Hsiao <phinganhsiao@gmail.com>
2022-02-21 18:02:00 -0500

    02/21/22

--- DisapperedNumbers.js
+++ DisapperedNumbers.js
@@ -4,7 +4,7 @@
 var findDisappearedNumbers = function(nums) {
     let sorted = nums.sort()
     var res = [];
-    for(let i=1;i<nums.length+1;i++){
+    for(let i=nums[0];i<nums.length+1;i++){
         console.log(sorted.indexOf(i))
         if(sorted.indexOf(i)===-1){
             
---
+++ PowerOfTwo.js
@@ -0,0 +1,12 @@
+
+
+
+const checker = (n) => {
+    let i = 1
+
+    while( i < n ) {
+        i *= 2
+    } 
+
+    return i === n
+}
